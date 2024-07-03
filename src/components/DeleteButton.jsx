8cd55b7ffd12d55d5ff5738b3207d02baab503f9
Rafael Luz <133050027+rafaeldesp@users.noreply.github.com>
2024-07-03 10:17:25 -0300

    Update DeleteButton.jsx

--- src/components/DeleteButton.jsx
+++ src/components/DeleteButton.jsx
@@ -1,20 +1,29 @@
-import proptypes from 'prop-types'
+import { useNavigate } from "react-router-dom"
+import useStock from "../hooks/useStock"
+import PropTypes from "prop-types"
 
 DeleteButton.propTypes = {
-    itemName: proptypes.string
+  itemId: PropTypes.number.isRequired,
+  itemName: PropTypes.string.isRequired
+}
+
+export default function DeleteButton({ itemId, itemName }) {
+  const { deleteItem } = useStock()
+  const navigate = useNavigate()
+
+  const handleDelete = () => {
+    if (confirm(`Tem certeza que deseja excluir ${itemName}?`)) {
+      deleteItem(itemId)
+      navigate("/items")
+    }
   }
 
-export default function DeleteButton({itemName}) {
-
-    const handleDelete = () => {
-        if (confirm(`Tem certeza que deseja excluir o ${itemName}`)) {
-            alert('Excluido.')
-        }
-    }
-    return (
-        <button className="button is-danger is-small"
-        onClick={handleDelete}>
-            Excluir
-        </button>
-    )
+  return (
+    <button
+    className="button is-danger is-small"
+    onClick={handleDelete}
+  >
+    Excluir
+  </button>
+  )
 }
