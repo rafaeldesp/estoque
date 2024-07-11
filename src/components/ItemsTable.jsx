c038b06ddd9bfa31afc74977e754480fac85ae61
rafael <rafael3luz@gmail.com>
2024-07-11 14:51:48 -0300

    mudança na responsividade

--- src/components/ItemsTable.jsx
+++ src/components/ItemsTable.jsx
@@ -6,35 +6,37 @@ export default function ItemsTable() {
     const { items } = useStock();
 
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nome</th>
-                    <th>Em Estoque</th>
-                    <th>Categoria</th>
-                    <th>Ações</th>
-                </tr>
-            </thead>
-            <tbody>
-                {items.map((item) => (
-                    <tr key={item.id}>
-                        <td>{item.id}</td>
-                        <td>{item.name}</td>
-                        <td>{item.quantity} unid.</td>
-                        <td>{item.category}</td>
-                        <td>
-                            <Link to={`/items/${item.id}`} className="button is-primary is-small">
-                                Ver
-                            </Link>
-                            <Link to={`/items/${item.id}/update`} className="button is-small">
-                                Atualizar
-                            </Link>
-                            <DeleteButton  itemName={item.name}/>
-                        </td>
+        <div className="formContainer">
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Em Estoque</th>
+                        <th>Categoria</th>
+                        <th>Ações</th>
                     </tr>
-                ))}
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    {items.map((item) => (
+                        <tr key={item.id}>
+                            <td>{item.id}</td>
+                            <td>{item.name}</td>
+                            <td>{item.quantity} unid.</td>
+                            <td>{item.category}</td>
+                            <td>
+                                <Link to={`/items/${item.id}`} className="button is-primary is-small">
+                                    Ver
+                                </Link>
+                                <Link to={`/items/${item.id}/update`} className="button is-small">
+                                    Atualizar
+                                </Link>
+                                <DeleteButton  itemName={item.name}/>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     )
 }
